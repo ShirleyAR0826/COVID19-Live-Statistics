30d1f83db85f030072587320a9ba81e23f35816a
Shirley <shirley.apellido@gmail.com>
2020-05-22 17:23:30 +0800

    fixing table header

--- src/Components/SearchBox/SearchBox.js
+++ src/Components/SearchBox/SearchBox.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 const SearchBox = ({searchfield, searchChange}) => {
     return(
-        <div className='pa2 '>
-            <input className='pa3 ba b--green bg-lightest-blue' 
-            type='search' 
-            placeholder='Search Country'
-            onChange={searchChange}
-            />
+        <div>
+            <div className="pt5">
+                <h2>By Country Summary</h2>
+            </div>
+            <div className='pa2'>
+                <input className='pa3 ba b--green bg-lightest-blue' 
+                type='search' 
+                placeholder='Search Country'
+                onChange={searchChange}
+                />
+            </div>
         </div>
     )
 }
--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,40 +1,36 @@
 import React from 'react';
 import './Table.css';
-import Scroll from '../Scroll/Scroll';
-
 
 const Table = ({Countries}) => {
     return(
         <div class="pa4">
             <div class="overflow-auto">
-                    <div class="f4 w-100 center dt dt--fixed" cellspacing="0">
-                        <tr>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tl">Country</th>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tc">New Confirmed</th>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tc">Total Confirmed</th>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tc">New Deaths</th>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tc">Total Deaths</th>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tc">New Recovered</th>
-                            <th class="f3 bb b--black-20 pb3 pr3 dtc tc">Total Recovered</th>
+                <table>
+                    <thead cellspacing="0">
+                        <tr className="dt dt--fixed w-100">
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tl">Country</th>
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tc">New Confirmed</th>
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tc">Total Confirmed</th>
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tc">New Deaths</th>
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tc">Total Deaths</th>
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tc">New Recovered</th>
+                            <th class="f3 bb b--black-20 pb3 pr2 dtc tc">Total Recovered</th>
                         </tr>
-                    </div>
-                <Scroll>
-                <table class="f4 w-100 center dt dt--fixed" cellspacing="0">
-                    <tbody>
+                    </thead>
+                    <tbody cellspacing="0">
                         {Countries.map((entry) => {
-                        return(<tr>
+                        return(<tr className="dt dt--fixed w-100">
                             <td class="pv3 pr3 bb b--black-20 dtc tl">{entry.Country}</td>
-                            <td class="pv3 pr3 bb b--black-20 dtc tc">{entry.NewConfirmed.toLocaleString()}</td>
-                            <td class="pv3 pr3 bb b--black-20 dtc tc">{entry.TotalConfirmed.toLocaleString()}</td>
-                            <td class="pv3 pr3 bb b--black-20 dtc tc">{entry.NewDeaths.toLocaleString()}</td>
-                            <td class="pv3 pr3 bb b--black-20 dtc tc">{entry.TotalDeaths.toLocaleString()}</td>
-                            <td class="pv3 pr3 bb b--black-20 dtc tc">{entry.NewRecovered.toLocaleString()}</td>
-                            <td class="pv3 pr3 bb b--black-20 dtc tc">{entry.TotalRecovered.toLocaleString()}</td>
+                            <td class="pv3 pr5 bb b--black-20 dtc tr">{entry.NewConfirmed.toLocaleString()}</td>
+                            <td class="pv3 pr5 bb b--black-20 dtc tr">{entry.TotalConfirmed.toLocaleString()}</td>
+                            <td class="pv3 pr5 bb b--black-20 dtc tr">{entry.NewDeaths.toLocaleString()}</td>
+                            <td class="pv3 pr5 bb b--black-20 dtc tr">{entry.TotalDeaths.toLocaleString()}</td>
+                            <td class="pv3 pr5 bb b--black-20 dtc tr">{entry.NewRecovered.toLocaleString()}</td>
+                            <td class="pv3 pr5 bb b--black-20 dtc tr">{entry.TotalRecovered.toLocaleString()}</td>
                         </tr>)}
                         )}
                     </tbody>
                 </table>
-                </Scroll>
             </div>
         </div>
     )
